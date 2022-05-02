d500f063e4847294fd352b2e13465b61b284b73f
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-05-02 15:58:32 -0300

    conectando back com front

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ var app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
+io.on("connection", (socket) => {
+    socket.on("disconnect", () => {
+        console.log("X desconectou: " + socket.id);
+    })
+});
+
 app.set("view engine","ejs");
 
 app.get("/", (req, res) => {
     res.render("index");
-})
+});
 
 http.listen(3000, () => {
     console.log("servidor rodando");
